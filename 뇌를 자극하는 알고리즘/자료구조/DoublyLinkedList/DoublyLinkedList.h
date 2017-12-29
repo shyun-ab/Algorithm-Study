a23125ad5cfbffadb21e6d2145f2bb19fecf6056
shyun-ab <shyun.ab@gmail.com>
2017-12-30 02:12:50 +0900

    Doubly Linked List Header

---
+++ 뇌를 자극하는 알고리즘/자료구조/DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,23 @@
+#ifndef DOUBLY_LINKEDLIST_H
+#define DOUBLY_LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagNode{
+	ElementType Data;
+	struct tagNode* PrevNode;
+	struct tagNode* NextNode;
+} Node;
+
+Node* CreateNode(ElementType NewData);
+void DestroyNode(Node* Node);
+void AppendNode(Node** Head, Node* NewNode);
+void InsertAfter(Node* Current, Node* NewNode);
+void RemoveNode(Node** Head, Node* Remove);
+Node* GetNodeAt(Node* Head, int Location);
+int GetNodeCount(Node* Head);
+
+#endif
