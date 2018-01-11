5a407ebdf2d9ab441684113802880adb0cb75637
shyun-ab <shyun.ab@gmail.com>
2018-01-11 23:41:12 +0900

    Stack code header

---
+++ 뇌를 자극하는 알고리즘/자료구조/Stack/LinkedListStack/LinkedListStack.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDLIST_STACK_H
+#define LINKEDLIST_STACK_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct tagNode {
+	char* Data;
+	struct tagNode* NextNode;
+} Node;
+
+typedef struct tagLinkedListStack
+{
+	Node* List;
+	Node* Top;
+} LinkedListStack;
+
+void CreateStack(LinkedListStack** Stack);
+void DestoryStack(LinkedListStack* Stack);
+
+Node* CreateNode(char* Data);
+void DestroyNode(Node* Node);
+
+void Push(LinkedListStack* Stack, Node* NewNode);
+Node* Pop(LinkedListStack* Stack);
+
+Node* Top(LinkedListStack* Stack);
+int GetSize(LinkedListStack* Stack);
+int isEmpty(LinkedListStack* Stack);
+
+#endif // !LINKEDLIST_STACK_H
