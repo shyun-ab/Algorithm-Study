79efcedb4658261dfc03722e96cd1e9e1a3ff0cd
shyun-ab <shyun.ab@gmail.com>
2018-01-24 00:23:27 +0900

    circular-queue-header

---
+++ 뇌를 자극하는 알고리즘/자료구조/Queue/CircularQueue.c
@@ -0,0 +1,11 @@
+#include "CircularQueue.h"
+
+void CreateQueue(CircularQueue** Queue, int Capacity) {
+
+}
+void DestroyQueue(CircularQueue* Queue);
+void Enqueue(CircualarQueue* Queue, ElementType Data);
+ElementType Dequeue(CircularQueue* Queue);
+int GetSize(CircularQueue* Queue);
+int IsEmpty(CircularQueue* Queue);
+int IsFull(CircularQueue* Queue);
---
+++ 뇌를 자극하는 알고리즘/자료구조/Queue/CircularQueue.h
@@ -0,0 +1,29 @@
+#ifndef CIRCULAR_QUEUE_H
+#define CIRCULAR_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagNode {
+	ElementType Data;
+} Node;
+
+typedef struct tagCircularQueue {
+	int Capacity;
+	int Front;
+	int Rear;
+
+	Node* Nodes;
+} CircularQueue;
+
+void CreateQueue(CircularQueue** Queue, int Capacity);
+void DestroyQueue(CircularQueue* Queue);
+void Enqueue(CircualarQueue* Queue, ElementType Data);
+ElementType Dequeue(CircularQueue* Queue);
+int GetSize(CircularQueue* Queue);
+int IsEmpty(CircularQueue* Queue);
+int IsFull(CircularQueue* Queue);
+
+#endif
