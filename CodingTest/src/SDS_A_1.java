0d3953b8f31113fe934739038b17551905a8ee59
shyun-ab <shyun.ab@gmail.com>
2018-07-20 00:52:18 +0900

    SDS Algorithm test A-type 1

--- CodingTest/src/SDS_A_1.java
+++ CodingTest/src/SDS_A_1.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class SDS_A_1 {
 
-	static int Answer;
-
 	public static void main(String args[]) throws Exception	{
 		
 		//Scanner sc = new Scanner(System.in);
@@ -14,18 +12,40 @@ public class SDS_A_1 {
 		
 		for(int test_case = 0; test_case < T; test_case++) {
 
-			Answer = 0;
+			int min = Integer.MAX_VALUE;
+			int temp;
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			int r1 = sc.nextInt();
-			int c1 = sc.nextInt();
-			int r2 = sc.nextInt();
-			int c2 = sc.nextInt();
+			int[][] src = new int[4][2];
+			int[][] dst = new int[4][2]; 
+			src[0][0] = sc.nextInt();
+			src[0][1] = sc.nextInt();
+			dst[0][0] = sc.nextInt();
+			dst[0][1] = sc.nextInt();
 			
+			src[1][0] = src[0][0] + N;
+			src[1][1] = src[0][1];
+			src[2][0] = src[0][0];
+			src[2][1] = src[0][1] + M;
+			src[3][0] = src[0][0] + N;
+			src[3][1] = src[0][1] + M;
 			
+			dst[1][0] = dst[0][0] + N;
+			dst[1][1] = dst[0][1];
+			dst[2][0] = dst[0][0];
+			dst[2][1] = dst[0][1] + M;
+			dst[3][0] = dst[0][0] + N;
+			dst[3][1] = dst[0][1] + M;
+			
+			for(int i = 0; i < 4; i++) {
+				for(int j = 0; j < 4; j++) {
+					temp = Math.abs(src[i][0] - dst[j][0]) + Math.abs(src[i][1] - dst[j][1]);
+					if(temp < min) min = temp;
+				}
+			}
 			
 			// Print the answer to standard output(screen).
-			System.out.println("#"+(test_case+1)+" "+Answer);
+			System.out.println("#"+(test_case+1)+" "+min);
 		}
 	}
 }
--- CodingTest/src/SDS_A_3.java
+++ CodingTest/src/SDS_A_3.java
@@ -1,86 +1,30 @@
-import java.io.FileInputStream;
 import java.util.*;
-
+ 
 public class SDS_A_3 {
-
-	static int Answer;
-	static int[][] info;
-	static int[] score;
-
-	public static void main(String args[]) throws Exception	{
-		
-		//Scanner sc = new Scanner(System.in);
-		Scanner sc = new Scanner(new FileInputStream("input.txt"));
-
-		int T = sc.nextInt();
-		
-		for(int test_case = 0; test_case < T; test_case++) {
-
-			Answer = 0;
-			int N = sc.nextInt();
-			int min = sc.nextInt();
-			int time = sc.nextInt();
-			info = new int[N][2];
-			score = new int[N];
-			
-			for(int i = 0; i < N; i++) {
-				info[i][0] = sc.nextInt();
-				info[i][1] = sc.nextInt();
-				score[i] = calculate(i+1, min, time);
-				if(score[i] > Answer) Answer = score[i];
-			}
-			
-			// Print the answer to standard output(screen).
-			System.out.println("#"+(test_case+1)+" "+Answer);
-		}
-	}
-	
-	public static int calculate(int num, int min, int time) {
-		int temp = time;
-		int[] Q = new int[3];
-		int[] tmpTime = new int[3];
-		
-		if((time / min) < num) return 0;
-		for(int i = 0; i < num; i++) {
-			temp = temp - info[i][0];
-		}
-		if(temp >= min) return 0;
-		
-		temp = 10*num;
-		Arrays.fill(Q, 0);
-		Arrays.fill(tmpTime, 0);
-		for(int i = 0; i < num; i++) {
-			temp = temp - info[i][0]*info[i][1];
-			if(info[i][1] == 3) {
-				Q[2] += (info[i][0] - min);
-				tmpTime[2] += min;
-			}
-			else if(info[i][1] == 2) {
-				Q[1] += (info[i][0] - min);
-				tmpTime[1] += min;
-			}
-			else {
-				Q[0] += (info[i][0] - min);
-				tmpTime[0] += min;
-			}
-		}
-		
-		time = time - min * num;
-		for(int i = 3; i > 0; i--) {
-			if(time > 0 && Q[i-1] != 0) {
-				if(Q[i-1] - time >= 0) {
-					tmpTime[i-1] += time;
-					time = 0;
-				}
-				else {
-					tmpTime[i-1] += Q[i-1];
-					time -= Q[i-1];
-				}
-				
-				temp = temp + tmpTime[i-1]*i;
-			}
-		}
-		
-		return temp;
-	}
+ 
+    static int Answer;
+ 
+    public static void main(String args[]) throws Exception {
+         
+        Scanner sc = new Scanner(System.in);
+         
+        int T = sc.nextInt();
+         
+        for(int test_case = 0; test_case < T; test_case++) {
+ 
+            Answer = 0;
+            int N = sc.nextInt();
+            int Start = sc.nextInt();
+            int Jump = sc.nextInt();
+            int K = sc.nextInt();
+            int[] rest = new int[K];
+             
+            for(int i = 0; i < K; i++) {
+                rest[i] = sc.nextInt();
+                if(rest[i] - Start >= 0 && ((rest[i] - Start) % (Jump + 1)) == 0) Answer++;
+            }
+             
+            System.out.println("#"+(test_case+1)+" "+Answer);
+        }
+    }
 }
